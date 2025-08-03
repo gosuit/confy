7df25ccfaa06d5e9574d4085d259df3450c6a31c
Nikita <ns_stepanov@inbox.ru>
2025-08-03 22:33:39 +0300

    Init confy constants

---
+++ variables.go
@@ -0,0 +1,30 @@
+package confy
+
+const (
+	// File tags
+	confyTag = "confy"
+	yamlTag  = "yaml"
+	jsonTag  = "json"
+	tomlTag  = "toml"
+
+	// Env tags
+	envTag          = "env"
+	envDefaultTag   = "env-default"
+	envLayoutTag    = "env-layout"
+	envSeparatorTag = "env-separator"
+	envRequiredTag  = "env-required"
+
+	// Common tags
+	defaultTag  = "default"
+	layoutTag   = "layout"
+	validateTag = "validate"
+
+	// Default values
+	defaultRootPath   = "config"
+	defaultEnvVarName = "ENVIRONMENT"
+	defaultSeparator  = ";"
+)
+
+var (
+	validExtensions = []string{".yaml", ".yml", ".json", ".toml"}
+)
